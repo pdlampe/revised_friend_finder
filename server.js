af364393905626610bacc710fbf0fd37a7db7bf6
Eli Vargas <eevargas@gmail.com>
2019-09-10 19:49:37 -0500

    Fix path on prod

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.text());
 
 app.use(express.static("./app/public"));
 
-require(path.join(__dirname, 'app/routing/apiRoutes.js'))(app);
-require(path.join(__dirname, 'app/routing/htmlRoutes.js'))(app);
+require(path.join(__dirname, './app/routing/apiRoutes.js'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
 
 app.listen(PORT, function () {
-    console.log("Application listening on PORT: " + PORT);
+    console.log("Application listening on PORT: http://localhost:" + PORT);
 });
